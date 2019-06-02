8bd4ba784dbe775b8348722a79b11b0e819b9c2a
Carlos Gutierrez Ramirez <dev0025@simelo.tech>
2019-06-02 03:31:09 +0000

    [ci] refs #112 - Linter rule no-plus-plus

--- ci-scripts/pre-release.genaddr.js
+++ ci-scripts/pre-release.genaddr.js
@@ -6,7 +6,7 @@ dw.setDeviceType(dw.DeviceTypeEnum.USB);
 
 dw.devAddressGen(5, 0).
   then((addresses) => {
-    for (i = 0; i < addresses.length; ++i) {
+    for (i = 0; i < addresses.length; i += 1) {
       console.log(addresses[i]);
     }
   });
