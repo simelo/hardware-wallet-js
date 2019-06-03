41b96df16e57936a02f327880c1dd6192f82e7ee
Carlos Gutierrez Ramirez <dev0025@simelo.tech>
2019-06-03 23:27:36 +0000

    [ci] refs #112 - Default value for device label if OS env var not defined

--- ci-scripts/pre-release.setlabel.js
+++ ci-scripts/pre-release.setlabel.js
@@ -4,9 +4,11 @@ dw = require("../skywallet");
 dw.setDeviceType(dw.DeviceTypeEnum.USB);
 
 const usePassphrase = false;
+const envLabel = process.env.ID3;
+const deviceLabel = (!envLabel || envLabel === '')? `MyWallet${process.pid}` : envLabel;
 console.log('Passphrase :', usePassphrase);
 
-dw.devApplySettings(usePassphrase, process.env.ID3).
+dw.devApplySettings(usePassphrase, deviceLabel).
   then(dw.devGetFeatures).
   then((features) => {
     console.log(features);
