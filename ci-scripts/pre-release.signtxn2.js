d2a265858f69b356cd3df47bf8ea49f3d6ef1f3a
Carlos Gutierrez Ramirez <dev0025@simelo.tech>
2019-06-08 16:39:38 +0000

    [ci] refs #112 - Pre-release recover seed for WALLET2 to sign all inputs in TXN3_JSON
    No details for TXN4_JSON

---
+++ ci-scripts/pre-release.signtxn2.js
@@ -0,0 +1,64 @@
+
+scanf = require('scanf');
+
+dw = require("../skywallet");
+
+dw.setDeviceType(dw.DeviceTypeEnum.USB);
+
+const usePassphrase = false;
+const address1 = process.env.ADDRESS1;
+const address2 = process.env.ADDRESS2;
+const address3 = process.env.ADDRESS3;
+const address4 = process.env.ADDRESS4;
+const envTxn = process.env.TXN3_JSON;
+const rTxn = JSON.parse(envTxn);
+
+console.log("Sender address", address1);
+console.log("Destination address", address2);
+console.log("Detected transaction JSON object", JSON.stringify(rTxn));
+console.log('Passphrase :', usePassphrase);
+
+const confirmPromise = function() {
+  return new Promise((resolve) => {
+    console.log("Continue? [yes]/no:");
+    answer = scanf("%s");
+    resolve(answer != 'no');
+  });
+};
+
+console.log("Please confirm that all data is correct");
+confirmPromise().
+  then((goFwd) => {
+    if (!goFwd) {
+      throw new Error('Process cancelled by user');
+    }
+    return dw.devAddressGen(6, 0);
+  }).
+  then((addresses) => {
+    let inputs = [];
+    let outputs = [];
+
+    for (i = 0; i < rTxn.inputs.length; i += 1) {
+      inputs.push({
+        'hashIn': rTxn.inputs[i],
+        'index': 0
+      });
+    }
+
+    for (i = 0; i < rTxn.outputs.length; i += 1) {
+      const out = rTxn.outputs[i];
+      outputs.push({
+        'address': out.dst,
+        'address_index': addresses.indexOf(out.dst),
+        'coin': out.coins.replace('.', ''),
+        'hour': out.hours
+      });
+    }
+
+    console.log('Sending inputs to device', JSON.stringify(inputs));
+    console.log('Sending outputs to device', JSON.stringify(outputs));
+    return dw.devSkycoinTransactionSign(inputs, outputs);
+  }).
+  then(console.log).
+  catch(console.log);
+
